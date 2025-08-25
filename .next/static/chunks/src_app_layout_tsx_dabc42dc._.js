fe1acab8e16de45b43389c2a71c036f89a33b58e
Michelle McGowan <mcgowanlmichelle@gmail.com>
2025-08-24 21:44:56 -0700

    Sidebar Done!
    - delete note button on sidebar w/ database deletion
    - select note button that shows only note that user selected
    - sidebar changes note prev to updated note

--- .next/static/chunks/src_app_layout_tsx_dabc42dc._.js
+++ .next/static/chunks/src_app_layout_tsx_dabc42dc._.js
@@ -2,10 +2,10 @@
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
   "static/chunks/src_styles_globals_92af3bb9.css",
-  "static/chunks/src_ae0e6583._.js",
+  "static/chunks/src_dbf6983f._.js",
   "static/chunks/9bf22_next_ec901535._.js",
   "static/chunks/8484d_tailwind-merge_dist_bundle-mjs_mjs_4d8e7d9e._.js",
-  "static/chunks/node_modules__pnpm_65a5c71a._.js"
+  "static/chunks/node_modules__pnpm_d9834b54._.js"
 ],
     source: "dynamic"
 });
